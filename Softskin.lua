9b55eca263925bfcc397c4364e010ac112cb81cf
joel.garboden <joel@valkyrnstudios.net>
2022-04-05 13:19:32 -0700

    Update formatting

--- Softskin.lua
+++ Softskin.lua
@@ -1,5 +1,4 @@
-Softskin = LibStub("AceAddon-3.0"):NewAddon("Softskin", "AceConsole-3.0",
-                                            "AceEvent-3.0")
+Softskin = LibStub("AceAddon-3.0"):NewAddon("Softskin", "AceConsole-3.0", "AceEvent-3.0")
 
 local playerGUID = UnitGUID("player")
 local playerClass, _ = UnitClassBase("player")
@@ -51,49 +50,58 @@ function Softskin:ChatCommand(input)
     end
 end
 
-function Softskin:OnEnable() self:EvaluateShaman() end
+function Softskin:OnEnable()
+    self:EvaluateShaman()
+end
 
-function Softskin:GROUP_ROSTER_UPDATE() self:EvaluateShaman() end
+function Softskin:GROUP_ROSTER_UPDATE()
+    self:EvaluateShaman()
+end
 
 function Softskin:EvaluateShaman()
-    local classFilename;
+    local classFilename
 
     for i = 1, GetNumGroupMembers() do
         classFilename, _ = UnitClassBase("party" .. i)
         if classFilename == "SHAMAN" then
-            self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
-            if DEBUG then self:SendMessage("Shaman detected") end
+            self:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+            if DEBUG then
+                self:SendMessage("Shaman detected")
+            end
             return
         end
     end
 
-    self:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
-    if DEBUG then self:SendMessage("No Shaman detected") end
+    self:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+    if DEBUG then
+        self:SendMessage("No Shaman detected")
+    end
 end
 
 function Softskin:COMBAT_LOG_EVENT_UNFILTERED(...)
-    if AuraUtil.FindAuraByName("Stoneskin", "player") ~= nil then
-        if AuraUtil.FindAuraByName("Strength of Earth", "player") ~= nil then
-            return -- Multiple shaman
-        end
-
-        return self:CombatLogHandler(CombatLogGetCurrentEventInfo())
-    else
-        return
-    end
+    return self:CombatLogHandler(CombatLogGetCurrentEventInfo())
 end
 
 function Softskin:CombatLogHandler(...)
     local _, subevent, _, _, _, _, _, destGUID, _, _, _ = ...
 
-    if destGUID ~= playerGUID or subevent ~= "SWING_DAMAGE" then return end
+    if destGUID ~= playerGUID or subevent ~= "SWING_DAMAGE" then
+        return
+    end
+
+    local _, _, _, _, _, _, _, _, buffID = AuraUtil.FindAuraByName("Stoneskin", "player")
+
+    if buffID ~= nil then
+        if AuraUtil.FindAuraByName("Strength of Earth", "player") ~= nil then
+            return -- Multiple shaman
+        end
+    end
 
     local amount, _, _, _, _, _, _, _, _, _ = select(12, ...)
 
     damageTaken = damageTaken + amount
 
-    local reduction = C_PaperDollInfo.GetArmorEffectivenessAgainstTarget(
-                          UnitArmor("player"))
+    local reduction = C_PaperDollInfo.GetArmorEffectivenessAgainstTarget(UnitArmor("player"))
 
     local softskinSwingDamage = amount / reduction
 
@@ -103,55 +111,46 @@ function Softskin:CombatLogHandler(...)
     damageMitigated = damageMitigated + (swingDamage - softskinSwingDamage)
 
     if DEBUG then
-        self:SendMessage(
-            "swingDamage: " .. swingDamage .. "; damageMitigated: " ..
-                damageMitigated)
+        self:SendMessage("swingDamage: " .. swingDamage .. "; damageMitigated: " .. damageMitigated)
     end
 end
 
 function Softskin:SendMessage(msg)
     if DEFAULT_CHAT_FRAME and msg ~= nil then
-        DEFAULT_CHAT_FRAME:AddMessage("Softskin Totem: " .. msg, 0.0, 1.0, 0.0,
-                                      1.0);
+        DEFAULT_CHAT_FRAME:AddMessage("Softskin Totem: " .. msg, 0.0, 1.0, 0.0, 1.0)
     end
 end
 
 function Softskin:BuildReport()
-    local classFilename, hasKings, entityName, unitAP;
+    local classFilename, hasKings, entityName, unitAP
 
-    local report = string.format(
-                       "Analysis\nDamage taken: %d\nMaximum mitigated: %d (%.2f%%)\nTheoretical loss of:\n",
-                       math.floor(damageTaken), math.floor(damageMitigated),
-                       damageMitigated / damageTaken * 100)
+    local report = string.format("Analysis\nDamage taken: %d\nMaximum mitigated: %d (%.2f%%)\nTheoretical loss of:\n",
+        math.floor(damageTaken), math.floor(damageMitigated), damageMitigated / damageTaken * 100)
 
     hasKings = AuraUtil.FindAuraByName("Blessing of Kings", "player") or
                    AuraUtil.FindAuraByName("Greater Blessing of Kings", "player")
 
-    report = report .. string.format('* %s: %d AP\n', playerName, math.floor(
-                                         self:GetEffectiveAP(playerClass,
-                                                             hasKings)))
+    report = report ..
+                 string.format('* %s: %d AP\n', playerName, math.floor(self:GetEffectiveAP(playerClass, hasKings)))
 
     for i = 1, GetNumGroupMembers() - 1 do
         classFilename, _ = UnitClassBase("party" .. i)
         entityName = UnitName("party" .. i)
 
         if entityName ~= nil then
-            hasKings =
-                AuraUtil.FindAuraByName("Blessing of Kings", "party" .. i) or
-                    AuraUtil.FindAuraByName("Greater Blessing of Kings",
-                                            "party" .. i)
+            hasKings = AuraUtil.FindAuraByName("Blessing of Kings", "party" .. i) or
+                           AuraUtil.FindAuraByName("Greater Blessing of Kings", "party" .. i)
 
             unitAP = math.floor(self:GetEffectiveAP(classFilename, hasKings))
 
             if unitAP > 0 then
-                report = report ..
-                             string.format('* %s: %d AP\n', entityName, unitAP)
+                report = report .. string.format('* %s: %d AP\n', entityName, unitAP)
             end
         end
 
     end
 
-    return report;
+    return report
 end
 
 function Softskin:Announce(report)
@@ -159,7 +158,9 @@ function Softskin:Announce(report)
 
     SendChatMessage("Softskin Totem: " .. lines[1], "PARTY")
 
-    for i = 2, #lines do SendChatMessage(lines[i], "PARTY") end
+    for i = 2, #lines do
+        SendChatMessage(lines[i], "PARTY")
+    end
 end
 
 function Softskin:GetEffectiveAP(class, hasKings)
